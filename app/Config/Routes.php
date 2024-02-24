735828ae9a5b6a70603c3ecb96f142425d6b006c
lamhotsimamora <lamhotsimamora12345@gmail.com>
2024-02-24 16:00:04 +0700

    +

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/users', 'Home::getUsers');
+$routes->post('/add-user', 'Home::addUser');
+$routes->post('/update-user', 'Home::updateUser');
+$routes->post('/delete-user', 'Home::deleteUser');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,44 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function getUsers(){
+        $userModel = new \App\Models\UserModel();
+
+        return json_encode($userModel->findAll());
+    }
+
+    public function addUser(){
+        $name = $this->request->getPost('name');
+        $email = $this->request->getPost('email');
+
+        $data = [
+            'name' => $name,
+            'email'    => $email,
+        ];
+        $userModel = new \App\Models\UserModel();
+        // Inserts data and returns inserted row's primary key
+         return $userModel->insert($data);
+    }
+
+    public function updateUser(){
+        $name = $this->request->getPost('name');
+        $email = $this->request->getPost('email');
+        $id = $this->request->getPost('id');
+
+        $data = [
+            'name' => $name,
+            'email'    => $email,
+        ];
+
+        $userModel = new \App\Models\UserModel();
+        return $userModel->update($id, $data);
+    }
+
+    public function deleteUser(){
+        $userModel = new \App\Models\UserModel();
+        $id = $this->request->getPost('id');
+
+        return $userModel->where('id', $id)->delete();
+    }
 }
